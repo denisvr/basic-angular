252798adc0d3b74f663d38bec1ad87d9d3a1487d
Denis Silva <denisvr01@gmail.com>
2022-03-10 13:13:36 +0000

    Added new examplee page of Data Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { rootRouterConfig } from './app.routes';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { APP_BASE_HREF } from '@angular/common';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './navigation/menu/menu.component';
@@ -10,6 +11,7 @@ import { FooterComponent } from './navigation/footer/footer.component';
 import { AboutComponent } from './company/about/about.component';
 import { ContactComponent } from './company/contact/contact.component';
 import { RouterModule } from '@angular/router';
+import { DataBindingComponent } from './demo/data-binding/data-binding.component';
 
 @NgModule({
   declarations: [
@@ -18,10 +20,12 @@ import { RouterModule } from '@angular/router';
     HomeComponent,
     FooterComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    DataBindingComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     [RouterModule.forRoot(rootRouterConfig, { useHash: false })]
   ],
   providers: [{provide: APP_BASE_HREF, useValue: '/'}],
---
+++ src/app/demo/data-binding/data-binding.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styles: []
+})
+export class DataBindingComponent {
+
+  public clickCounter: number = 0;  
+  public urlImage: string = "https://angular.io/assets/images/logos/angular/angular.svg";
+  public name: string = "";
+
+  addClick(){
+    this.clickCounter++;
+  }
+
+  resetCounter(){
+    this.clickCounter = 0;
+  }
+
+  /* KeyUp(event: any){
+    this.name = event.target.value;
+  } */
+
+}
